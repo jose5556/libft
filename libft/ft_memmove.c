cfa8252dcc507d46af8fcf5be4a92243c0710301
jose5556 <josefilmeneses@gmail.com>
2024-02-25 15:54:52 +0000

    ft_memmove done

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,71 +1,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-char    *ifoverlap(void *dest, const void *src, size_t n, int *ptr, char *temporary);
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	int	i;
-	size_t	counter;
-	char	*temporary;
-	int	bolean;
-	int	*ptr;
-
-	bolean = 0;
-	ptr = &bolean;
-	counter = n;
-	i = 0;
-
-	write(1, "v", 1);
-	temporary = ifoverlap(dest, src, n, ptr, temporary);
-	write(1, "a", 1);
-	if (*ptr == 1)
-	{
-		while (n)
-		{
-			((char *)dest)[i] = temporary[i];
-			 i++;
-			 n--;
-		}
-	}
-	else
-	{
-		while (n)
-                {
-                        ((char *)dest)[i] = temporary[i];
-                         i++;
-                         n--;
-                }
-	}
-}
-
-char	*ifoverlap(void *dest, const void *src, size_t n, int *ptr, char *temporary)
-{
-	int	i;
-	size_t	counter;
+	size_t	i;
+	char	temporary[n];
 
 	i = 0;
-	counter = n;
-	while (((char *)dest)[i] == '\0')
-        {
+	while (i < n)
+	{
+		temporary[i] = ((char *)src)[i];
 		i++;
-		counter--;
-                if (((char *)dest)[i] != '\0' && counter > 0)
-                {
-			write(1, "a", 1);
-                        i = 0;
-                        counter = n;
-                        while (counter)
-                        {
-                                temporary[i] = ((char *)src)[i];
-                                i++;
-                                counter--;
-                        }
-                        *ptr = 1;
-                        return (temporary);
-                }
-        }
-	return (((char *)src));
+	}
+	i = 0;
+	while (temporary[i])
+	{
+		((char *)dest)[i] = temporary[i];
+		i++;
+	}
 }
 
 int	main(void)

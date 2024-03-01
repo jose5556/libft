57c2d5aa3f2a29faec9c08182b60d7b458025f06
jose5556 <josefilmeneses@gmail.com>
2024-03-01 18:10:13 +0000

    strchr done

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,4 +1,20 @@
-char *strchr(const char *s, int c)
-{
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+char *ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	s[3] = "era";
+	printf("%s\n", ft_strchr(s, 'r'));
 }

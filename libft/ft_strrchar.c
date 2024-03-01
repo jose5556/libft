e6f32bc299546b0ed4a80fddee7a502fec1657da
jose5556 <josefilmeneses@gmail.com>
2024-03-01 19:06:25 +0000

    strrchar done

---
+++ libft/ft_strrchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = ft_strlen(s);
+	s += len;
+	while (len >= 0)
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		if (len - 1 == 0 && s[0] != c)
+		{
+			return (0);
+		}
+		len--;
+		s--;
+	}
+}
+
+int	main(void)
+{
+	char	s[5] = "abcab";
+	printf("%s\n", ft_strrchr(s, 'a'));
+	//printf("%s\n", strrchr(s, 'i'));
+}

25ef36b2e0d85477456765b0b209b6e39c223a59
jose5556 <josefilmeneses@gmail.com>
2024-03-01 20:35:50 +0000

    strncmp done

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	i;
 
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_strncmp("abcd", "abcdf", 5));
+	printf("%d\n", strncmp("abcd", "abcdf", 5));
 }

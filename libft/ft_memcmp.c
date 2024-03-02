25cb3da22838c83b71baec3d14c10102fe344d65
jose5556 <josefilmeneses@gmail.com>
2024-03-02 21:50:55 +0000

    memcmp done

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,15 @@
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2[i]))
+		{
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2[i]))
+		}
+		i++;
+	}
+	return (0);
+}

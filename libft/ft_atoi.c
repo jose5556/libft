642fd7aa4ee1fc358b6d95fd45df827069e68935
jose5556 <josefilmeneses@gmail.com>
2024-03-06 19:51:44 +0000

    atoi done

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,4 +1,37 @@
-int	atoi(const char *nptr)
-{
+#include <stdlib.h>
+#include <stdio.h>
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	result;
+	int	check_value;
+
+	result = 0;
+	check_value = 1;
+	i = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+	{
+		i++;
+	}
+	if (nptr[i] == '-')
+	{
+		check_value *= -1;
+		i++;
+	}
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	printf("%d\n", result * check_value);
+	return (result * check_value);
+}
+
+int	main(void)
+{
+	ft_atoi("  	a-1234ppp");
+	printf("%d\n", atoi("       a-1234ppp"));
 }

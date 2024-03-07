19a2a4b4c2e8961881e7a0bc06136706339a1533
jose5556 <josefilmeneses@gmail.com>
2024-03-07 23:01:45 +0000

    strnstr done

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,45 @@
+/*
+ * The strnstr() function locates the first	occurrence of the  null-termi-
+       nated  string little in the string big, where not more than len characters are
+       searched.  Characters that appear after	a `\0'	character  are
+       not  searched.
+*/
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (little[0] == '\0')
+		return (((char *)big));
+	while (len > 0)
+	{
+		while (big[i] == little[j])
+		{
+			i++;
+			j++;
+			len--;
+			if (len == 0)
+                                return (NULL);
+			if (little[j + 1] == '\0')
+				return (((char *)big));
+		}
+		i = 0;
+		j = 0;
+		big++;
+		len--;
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	char big[12] = "era uma vez";
+	char little[4] = "uma";
+
+	printf("%s\n", ft_strnstr(big, little, 7));
+}
--- libft/libft.h
+++ libft/libft.h
@@ -16,7 +16,7 @@ int     ft_isascii(int c);
 
 int     ft_isprint(int c);
 
-size_t     ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 
 void    *ft_memset(void *s, int c, size_t n);
 
@@ -24,7 +24,7 @@ void    ft_bzero(void *s, size_t n);
 
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 
-void	*memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t	ft_strlcpy(char *dest, char *src, size_t n);
 
@@ -34,19 +34,19 @@ int     ft_toupper(int c);
 
 int     ft_tolower(int c);
 
-char	*strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
-char	*strrchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
-int	strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
-void	*memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
-int	memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char	*strnstr(const char *big, const char *little, size_t len)
+char    *ft_strnstr(const char *big, const char *little, size_t len);
 
-int	atoi(const char *nptr);
+int	ft_atoi(const char *nptr);
 
 //void *calloc(size_t nmemb, size_t size);
 

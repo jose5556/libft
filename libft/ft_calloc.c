5c85ab5eddba71afed4902daa95f73d5e0405710
jose5556 <josefilmeneses@gmail.com>
2024-04-10 18:46:07 +0100

    calloc fixed

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -2,34 +2,42 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
+	void    *ptr;
+	int	i;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	else if (nmemb * size > 2147483647)
-		return (NULL);
-	void    *ptr;
 	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
 	return (ptr);
 }
-/*
+
 int	main(void)
 {
 	int	i;
 	i = 0;
 
-	char	*str = (char*) ft_calloc(3, sizeof(char));
-	char    *strr = (char*) calloc(3, sizeof(char));
+	char	*str = (char*) ft_calloc(10, sizeof(char));
+	//char    *strr = (char*) calloc(10, sizeof(char));
 	
-	while (i < 3 * sizeof(char))
+	while (i < 10 * sizeof(char))
 	{
 		str[i] = 'a';
 		i++;
 	}
 	printf("%zu", ft_strlen(str));
-	while (i < 3 * sizeof(char))
+	/*
+	while (i < 40 * sizeof(char))
         {
                 strr[i] = 'a';
                 i++;
         }
-	printf("%zu", ft_strlen(strr));
+	printf("%zu", ft_strlen(strr));*/
 	return (0);
-}*/
+}

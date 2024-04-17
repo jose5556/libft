90e3c3ba3961df3b2aa19095eb22c00db5c8a5bb
jose5556 <josefilmeneses@gmail.com>
2024-04-17 20:20:12 +0100

    strncmp accepts unsigned char

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 16:37:58 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/14 16:38:01 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/17 20:19:01 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while ((str1[i] || str2[i]) && i < n)
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
 	}
 	return (0);
 }
 
 /* int	main(void)
 {
-	printf("%d\n", ft_strncmp("abcd", "abcdf", 5));
-	printf("%d\n", strncmp("abcd", "abcdf", 5));
-}
- */
+	printf("%d\n", ft_strncmp("abcdef", "abc\375xx", 5));
+	printf("%d\n", strncmp("abcdef", "abc\375xx", 5));
+} */

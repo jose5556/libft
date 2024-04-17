e8da8e0e13d6cf96f21e2fed48edbc1ef6842b61
jose5556 <josefilmeneses@gmail.com>
2024-04-17 19:01:27 +0100

    putnbr_fd done

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joseoliv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 19:35:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/14 19:35:14 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:00:37 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putstr_fd("-2147483648", 1);
+		ft_putstr_fd("-2147483648", fd);
 		return ;
 	}
 	if (n < 0)
 	{
-		ft_putchar_fd('-', 1);
+		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
 	if (n > 9)
 		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + 48, 1);
+	ft_putchar_fd(n % 10 + 48, fd);
 }
 
 /* int	main(void)
 {
-	ft_putnbr_fd(-2147483648, 1);
-	return (0);
+	ft_putnbr_fd(4, 1);
 } */

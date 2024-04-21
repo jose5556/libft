fb13e873724955c674f76a4fc9295c8289d69290
jose5556 <josefilmeneses@gmail.com>
2024-04-21 17:59:52 +0100

    tests done

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 23:13:37 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/21 16:36:54 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:59:27 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,24 @@ int	ft_lstsize(t_list *lst)
 	i = 0;
 	if (!lst)
 		return (0);
-	while (!lst)
+	while (lst)
 	{
 		i++;
 		lst = lst->next;
 	}
 	return (i);
 }
+
+/* int	main(void)
+{
+	t_list	*list1;
+	t_list	*list2;
+	t_list	*list3;
+	
+	list1 = ft_lstnew("era");
+	list2 = ft_lstnew("uma");
+	list3 = ft_lstnew("vez");
+	list1->next = list2;
+	list2->next = list3;
+	printf("%d", ft_lstsize(list1));
+} */

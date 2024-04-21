e25f4bf32237af8a4f62878a69a0cbfda0d15685
jose5556 <josefilmeneses@gmail.com>
2024-04-21 18:08:43 +0100

    complete

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 16:37:39 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/21 16:42:41 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/21 18:08:15 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,25 @@ t_list *ft_lstlast(t_list *lst)
 {
     if (!lst)
         return (NULL);
-    while (!lst->next)
+    while (lst->next)
         lst = lst->next;
     return (lst);
 }
+
+/* int main(void)
+{
+    t_list	*list1;
+	t_list	*list2;
+	t_list	*list3;
+    t_list  *result;
+	
+	list1 = ft_lstnew("era");
+	list2 = ft_lstnew("uma");
+	list3 = ft_lstnew("vez");
+    
+	list1->next = list2;
+	list2->next = list3;
+
+    result = ft_lstlast(list1);
+    printf("%s", (char *)result->content);
+} */

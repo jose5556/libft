d50ff15873d73eccd6792772f1f86e3e4f6b9642
jose5556 <josefilmeneses@gmail.com>
2024-04-21 15:13:22 +0100

    var name corrected

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,16 +6,16 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:58:13 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/20 23:12:10 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/21 15:12:46 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst)
-		return (*lts = new);
+		return (*lst = new);
 	new->next = *lst;
 	*lst = new;
 }

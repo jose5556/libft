c07595705898dabe5603ae2e922a84ff37f2a361
jose5556 <josefilmeneses@gmail.com>
2024-04-21 16:45:05 +0100

    new funcs added

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 16:57:20 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/20 23:13:23 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/21 16:38:16 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif

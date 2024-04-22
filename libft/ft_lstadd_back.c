2b7a70ebafd713af5a6e8defe9091ec830bf4d8d
jose5556 <josefilmeneses@gmail.com>
2024-04-22 15:12:22 +0100

    norminette fixed

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,28 +6,28 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:27:34 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/04/21 20:04:26 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:09:30 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    if (!ft_lstlast(*lst))
-        *lst = ft_lstnew(new);
-    else
-    {
-        ft_lstlast(*lst)->next = ft_lstnew(new);
-    }
+	if (!ft_lstlast(*lst))
+		*lst = ft_lstnew(new);
+	else
+	{
+		ft_lstlast(*lst)->next = ft_lstnew(new);
+	}
 }
 
-int	main(void)
+/* int	main(void)
 {
 	t_list	*list;
 
 	list = ft_lstnew("era");
-    ft_lstadd_back(&list, ft_lstnew("new"));
+	ft_lstadd_back(&list, ft_lstnew("new"));
 	
-    printf("%s", (char *)ft_lstlast(list));
-}
+	printf("%s", (char *)ft_lstlast(list));
+} */

fffc610be7525ebbbb47381f62ab12307e354cbd
jose5556 <josefilmeneses@gmail.com>
2024-11-25 18:03:57 +0000

    added a flag in prototype

--- string/ft_strjoin_free.c
+++ string/ft_strjoin_free.c
@@ -6,17 +6,20 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 03:33:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/10/11 04:11:51 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/25 18:03:19 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	*ft_strjoin_free(char *s1, char *s2)
+char	*ft_strjoin_free(char *s1, char *s2, int i)
 {
 	char	*temp;
 
 	temp = ft_strjoin(s1, s2);
-	free(s1);
+	if (i == 1)
+		free(s1);
+	if (i == 2)
+		free(s1);
 	return (temp);
 }

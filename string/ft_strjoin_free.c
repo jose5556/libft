402ed40d696895d571fbcce874f55b5f85dad4fc
jose5556 <josefilmeneses@gmail.com>
2024-11-29 00:11:59 +0000

    extra verifications and frees the var dependend of the flag

--- string/ft_strjoin_free.c
+++ string/ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 03:33:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/25 18:03:19 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/11/29 00:11:19 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@ char	*ft_strjoin_free(char *s1, char *s2, int i)
 {
 	char	*temp;
 
+	if (!s1 || !s2)
+        return (NULL);
 	temp = ft_strjoin(s1, s2);
+	if (!temp)
+        return (NULL);
 	if (i == 1)
 		free(s1);
-	if (i == 2)
+	else if (i == 2)
+		free(s2);
+	else
+	{
 		free(s1);
+		free(s2);
+	}
 	return (temp);
 }

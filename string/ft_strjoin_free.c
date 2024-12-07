06ac33632b232217000009d35636af752bb28e52
jose5556 <josefilmeneses@gmail.com>
2024-12-07 10:38:52 +0000

    norminette

--- string/ft_strjoin_free.c
+++ string/ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 03:33:18 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/11/29 00:11:19 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:38:26 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strjoin_free(char *s1, char *s2, int i)
 	char	*temp;
 
 	if (!s1 || !s2)
-        return (NULL);
+		return (NULL);
 	temp = ft_strjoin(s1, s2);
 	if (!temp)
-        return (NULL);
+		return (NULL);
 	if (i == 1 && s1)
 		free(s1);
 	else if (i == 2 && s2)
